8feb0f333887ab23692b60baf9e0cc35236a2b7d
rianhickey <rch27@tamu.edu>
2026-09-22 08:35:40 -0500

    First working version of simple yolo model

--- YOLO/yoloTEST.py
+++ YOLO/yoloTEST.py
@@ -1,1 +1,31 @@
+import torch
 
+
+from ultralytics import YOLO
+
+
+def main():
+	model = YOLO("yolov8n.pt")
+	filename = "yolo_test1.png"
+	path = "test_images/" + filename
+	#best practice to robustly define the device
+	#so it only attempts to utalize cuda if a cuda gpu is present
+	device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+	testResults1 = model(path) #passes the filename (path) to the model
+
+
+	for result in testResults1:
+  		boxes = result.boxes
+  		masks = result.masks
+  		keypoints = result.keypoints
+  		probs = result.probs
+  		print(boxes)
+  		print(masks)
+  		print(keypoints)
+  		print(probs)
+  		result.save(filename = "result_images/" + filename)
+
+
+if __name__ == "__main__":
+	main()
