733104d730f76ad84ee574e71bb948d70bbd3702
rian <rch27@tamu.edu>
2026-09-22 08:54:35 -0500

    Added feature to test every image inside of the test image folder

--- YOLO/yoloTEST.py
+++ YOLO/yoloTEST.py
@@ -1,31 +1,31 @@
 import torch
-
+import os #allows for looping through the entire directory of test images
 
 from ultralytics import YOLO
 
 
 def main():
 	model = YOLO("yolov8n.pt")
-	filename = "yolo_test1.png"
-	path = "test_images/" + filename
+
+	path = "test_images/"
 	#best practice to robustly define the device
 	#so it only attempts to utalize cuda if a cuda gpu is present
 	device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-	testResults1 = model(path) #passes the filename (path) to the model
+	for filename in os.listdir(path):
+		curr_path = os.path.join(path, filename)
+		testResults1 = model(curr_path) #passes the filename (path) to the model
 
-
-	for result in testResults1:
-  		boxes = result.boxes
-  		masks = result.masks
-  		keypoints = result.keypoints
-  		probs = result.probs
-  		print(boxes)
-  		print(masks)
-  		print(keypoints)
-  		print(probs)
-  		result.save(filename = "result_images/" + filename)
-
+		for result in testResults1:
+  			boxes = result.boxes
+  			masks = result.masks
+  			keypoints = result.keypoints
+  			probs = result.probs
+  			print(boxes)
+  			print(masks)
+  			print(keypoints)
+  			print(probs)
+  			result.save(filename = "result_images/" + filename)
 
 if __name__ == "__main__":
 	main()
